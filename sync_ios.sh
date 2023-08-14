65e0f897edffc3a91d00cd6930ab519316da103d
tangs <tangsong1988kaka@gmail.com>
2023-08-14 10:55:05 +0800

    update sh.

--- sync_ios.sh
+++ sync_ios.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 #cp -rf build/ios/Release-iphoneos/libwebm_unpacker.a ~/Documents/fish3/Assets/Third/Libvpx/ios/
-cp -rf build/ios/Debug-iphoneos/libwebm_unpacker.a ~/Documents/fish3/Assets/Third/Libvpx/ios/
+cp -rf build/ios/Release-iphoneos/libwebm_unpacker.a ~/Documents/fish3/Assets/Third/Libvpx/ios/
